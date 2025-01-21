1aafe2d1866bb1a21c03de708d40cea869fd8fee
Giordan Garcia <giordangarcia12@gmail.com>
2025-01-20 21:27:25 -0300

    Project hr-payroll created

---
+++ hr-payroll/src/main/java/com/giordan/hrpayroll/SpringPayrollApplication.java
@@ -0,0 +1,13 @@
+package com.giordan.hrpayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPayrollApplication.class, args);
+	}
+
+}
