d7ee8e89bd0cbde1cea00b50db7a894182f2528b
Giordan Garcia <giordangarcia12@gmail.com>
2025-01-21 23:08:18 -0300

    configuring eureka server

--- hr-worker/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO tb_worker (name, daily_income) VALUES ('Bob', 200.0);
-INSERT INTO tb_worker (name, daily_income) VALUES ('Maria', 300.0);
-INSERT INTO tb_worker (name, daily_income) VALUES ('Alex', 250.0);
--- hr-worker/src/main/resources/schema.sql
+++ hr-worker/src/main/resources/schema.sql
@@ -1,5 +1,9 @@
 CREATE TABLE tb_worker (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
-    daily_income DOUBLE
+    name VARCHAR(255) NOT NULL,
+    daily_income DOUBLE NOT NULL
 );
+
+INSERT INTO tb_worker (name, daily_income) VALUES ('Bob', 200.0);
+INSERT INTO tb_worker (name, daily_income) VALUES ('Maria', 300.0);
+INSERT INTO tb_worker (name, daily_income) VALUES ('Alex', 250.0);
