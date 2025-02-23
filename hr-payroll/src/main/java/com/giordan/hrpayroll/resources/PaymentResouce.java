e1fc953f6db5040e7a1ebc2a0a2383f8d03026d3
Giordan Garcia <giordangarcia12@gmail.com>
2025-02-23 17:34:11 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/com/giordan/hrpayroll/resources/PaymentResouce.java
+++ hr-payroll/src/main/java/com/giordan/hrpayroll/resources/PaymentResouce.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.http.ResponseEntity;
 import lombok.RequiredArgsConstructor;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,9 +17,17 @@ public class PaymentResouce {
     
     private final PaymentService paymentService;
 
+
+    @CircuitBreaker(name = "defaultCB", fallbackMethod = "getPaymentAlternative")
     @GetMapping("/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable long workerId, @PathVariable int days) {
         Payment payment = paymentService.getPayment(workerId, days);
         return ResponseEntity.ok(payment);
     }
+
+    public ResponseEntity<Payment> getPaymentAlternative(long workerId, int days, Throwable throwable) {
+        Payment paymentFallback = new Payment("Fallback Worker", 0.0, 0);
+        return ResponseEntity.ok(paymentFallback);
+    }
+    
 }
